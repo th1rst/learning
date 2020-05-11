6567a8cc67709e66812ac2b3d457a40bc037c83d
marco <marco@pc.fritz.box>
2020-05-11 23:56:23 +0200

    finised exercise, added readme.md

--- 06_Lodash_recreate/main.js
+++ 06_Lodash_recreate/main.js
@@ -1,34 +1,95 @@
 const _ = {
+  clamp(number, lower, upper) {
+    let lowerClampedValue = Math.max(number, lower);
+    let clampedValue = Math.min(lowerClampedValue, upper);
+    return clampedValue;
+  },
 
-    clamp(number, lower, upper) {
-      var lowerClampedValue = Math.max(number, lower);
-      var clampedValue = Math.min(lowerClampedValue, upper);
-      return clampedValue;
-    },
-  
-    inRange(number, start, end) {
-      if (end === undefined) {
-        end = start;
-        start = 0;
-      };
-      if (start > end) {
-        var temp = end;
-        end = start;
-        start = temp;
+  inRange(number, start, end) {
+    if (end === undefined) {
+      end = start;
+      start = 0;
+    }
+    if (start > end) {
+      let temp = end;
+      end = start;
+      start = temp;
+    }
+    let isInRange = start <= number && number < end;
+    return isInRange;
+  },
+
+  words(string) {
+    let words = string.split(" ");
+    return words;
+  },
+
+  pad(string, length) {
+    if (length <= string.length) {
+      return string;
+    }
+    let startPaddingLength = Math.floor((length - string.length) / 2);
+    let endPaddingLength = length - string.length - startPaddingLength;
+    let paddedString =
+      " ".repeat(startPaddingLength) + string + " ".repeat(endPaddingLength);
+    return paddedString;
+  },
+
+  has(object, key) {
+    let hasValue = object[key];
+    if (hasValue != undefined) {
+      return true;
+    }
+    return false;
+  },
+
+  invert(object) {
+    let invertedObject = {};
+    for (let key in object) {
+      let originalValue = object[key];
+      invertedObject = { originalValue: key };
+    }
+    return invertedObject;
+  },
+
+  findKey(object, predicate) {
+    for (let key in object) {
+      let value = object[key];
+      let predicateReturnValue = predicate(value);
+      if (predicateReturnValue) {
+        return key;
       }
-      var isInRange = start <= number && number < end;
-      return isInRange;
-    },
-  
-    words(string) {
-      var words = string.split(" ");
-      return words;
-    },
- 
-  
-  };
-  
-  
-  
+    }
+    undefined;
+    return undefined;
+  },
 
-  module.exports = _;
+  drop(array, n) {
+    if (n === undefined) {
+      n = 1;
+    }
+    let droppedArray = array.slice(n, array.length);
+    return droppedArray;
+  },
+
+  dropWhile(array, predicate) {
+    const cb = (element, index) => {
+      return !predicate(element, index, array);
+    };
+    let dropNumber = array.findIndex(cb);
+    let droppedArray = this.drop(array, dropNumber);
+    return droppedArray;
+  },
+
+  chunk(array, size = 1) {
+    let arrayChunks = [];
+    for (let i = 0; i < array.length; i += size) {
+      let arrayChunk = array.slice(i, i + size);
+      arrayChunks.push(arrayChunk);
+    }
+    return arrayChunks;
+  },
+};
+
+
+module.exports = _;
---
+++ 06_Lodash_recreate/readme.md
@@ -0,0 +1,3 @@
+This exercise is part of the Codecademy curriculum.
+
+For training purposes, some methods in _.js (main.js) are slightly modified compared to the original.
