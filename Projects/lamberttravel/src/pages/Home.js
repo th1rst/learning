5935f1d3cf59c91cf46a8ae5836c469a0c9d1068
th1rst <marco@kochannek.com>
2020-06-20 20:20:12 +0200

    added 'featured rooms' on /home

--- Projects/lamberttravel/src/pages/Home.js
+++ Projects/lamberttravel/src/pages/Home.js
@@ -76,9 +76,47 @@ export default function Home() {
       </div>
 
       <div className="room-card-container">
-        <div className="room-card"></div>
-        <div className="room-card"></div>
-        <div className="room-card"></div>
+        <div className="room-card">
+          <img
+            className="room-card-image"
+            src="https://images.unsplash.com/photo-1512918728675-ed5a9ecdebfd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+          ></img>
+          <div className="room-card-pricetag">
+            <h4>$123</h4>
+            <h5>per night</h5>
+          </div>
+          <div className="room-image-subtitle-bar">
+            <div className="room-image-subtitle-bar-text">Double Economy</div>
+          </div>
+        </div>
+        <div className="room-card">
+          <img
+            className="room-card-image"
+            src="https://images.unsplash.com/photo-1568495248636-6432b97bd949?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80"
+          ></img>
+          <div className="room-card-pricetag">
+            <h4>$234</h4>
+            <h5>per night</h5>
+          </div>
+          <div className="room-image-subtitle-bar">
+            <div className="room-image-subtitle-bar-text">Double Standard</div>
+          </div>
+        </div>
+        <div className="room-card">
+          <div className="room-card-pricetag">
+            <h4>$345</h4>
+            <h5>per night</h5>
+          </div>
+          <img
+            className="room-card-image"
+            src="https://images.unsplash.com/flagged/photo-1556438758-8d49568ce18e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80"
+          ></img>
+          <div className="room-image-subtitle-bar">
+            <div className="room-image-subtitle-bar-text">
+              Beachfront Luxurious
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
