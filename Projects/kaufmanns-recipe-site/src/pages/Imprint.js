d3513858946611a6e11c56117bef720296f33692
th1rst <marco@kochannek.com>
2020-07-24 02:18:54 +0200

    finished imprint page (sample names)

--- Projects/kaufmanns-recipe-site/src/pages/Imprint.js
+++ Projects/kaufmanns-recipe-site/src/pages/Imprint.js
@@ -9,7 +9,44 @@ export default function Imprint() {
       <BackgroundImage />
       <Navigation />
       <div className="data-privacy-container">
-        <h1>Hello from Imprint</h1>
+        <h1>Impressum</h1>
+        <h1>Angaben gemäß §5 TMG</h1>
+
+        <h2>Kontakt</h2>
+        <p>Max Mustermann</p>
+        <p>Musterstraße 111</p>
+        <p>90210 Musterstadt</p>
+        <p>Telefon: +49 (0) 123 44 55 66</p>
+        <p>E-Mail: mustermann@musterfirma.de</p>
+        <h2>Haftung für Inhalte</h2>
+        <p>
+          Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte
+          auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach
+          §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht
+          verpflichtet, übermittelte oder gespeicherte fremde Informationen zu
+          überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige
+          Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der
+          Nutzung von Informationen nach den allgemeinen Gesetzen bleiben
+          hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem
+          Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei
+          Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese
+          Inhalte umgehend entfernen.
+        </p>
+
+        <h2>Haftung für Links</h2>
+        <p>
+          Unser Angebot enthält Links zu externen Websites Dritter, auf deren
+          Inhalte wir keinen Einfluss haben. Deshalb können wir für diese
+          fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der
+          verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der
+          Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der
+          Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige
+          Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine
+          permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne
+          konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei
+          Bekanntwerden von Rechtsverletzungen werden wir derartige Links
+          umgehend entfernen.
+        </p>
       </div>
       <Footer />
     </div>
